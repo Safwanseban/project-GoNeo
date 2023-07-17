e3cefab2987aac81156d71a1612732be576e1762
Safwanseban <safwansappu0@gmail.com>
2023-07-18 01:42:11 +0530

    initialized logger to debug

--- internal/handlers/companyHandler.go
+++ internal/handlers/companyHandler.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/Safwanseban/voixme-project/configs"
 	"github.com/Safwanseban/voixme-project/internal/types"
 	"github.com/Safwanseban/voixme-project/internal/usecases"
 	"github.com/gofiber/fiber/v2"
@@ -34,9 +36,10 @@ func NewServer(app *fiber.App, companyUsecase usecases.UsecasesCompany) {
 }
 
 func (s *Server) Create(ctx *fiber.Ctx) error {
+	var logger = configs.Getlogger()
 	company := new(types.OfferCompany)
 	if err := ctx.BodyParser(company); err != nil {
-
+		logger.Error().Err(err).Send()
 		return ctx.Status(http.StatusBadRequest).JSON(
 			fiber.Map{
 				"message": "bad request",
@@ -45,6 +48,7 @@ func (s *Server) Create(ctx *fiber.Ctx) error {
 	}
 	id, err := s.ProductUseCase.CreateProduct(company)
 	if err != nil {
+		logger.Error().Err(err).Send()
 		return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
 			"message": "server error",
 		})
@@ -54,9 +58,10 @@ func (s *Server) Create(ctx *fiber.Ctx) error {
 }
 
 func (s *Server) Fetch(ctx *fiber.Ctx) error {
-
+	var logger = configs.Getlogger()
 	country := ctx.Query("country")
 	if country == "" {
+		logger.Error().Err(errors.New("invalid query")).Send()
 		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
 			"message": "provide valid country parameter",
 		})
@@ -65,6 +70,7 @@ func (s *Server) Fetch(ctx *fiber.Ctx) error {
 	company.Country = country
 	companies, err := s.ProductUseCase.ShowOfferCompany(&company)
 	if err != nil {
+		logger.Error().Err(err).Send()
 		return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
 			"message": "error fetching the records",
 		})
